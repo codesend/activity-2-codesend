6a60caa29a257830bbc05995500fdc933243df09
Daniel Krause <122369776+codesend@users.noreply.github.com>
2024-03-13 03:03:40 -0400

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,17 @@
 def wordCount(t):
+    dictionary = {}
+
+    file = open(t, 'r')
+    lines = file.readlines()
+    file.close()
+
+    line_num = 1
+    for line in lines:
+        words = line.strip().split()
+        for word in words:
+            if word not in dictionary.keys():
+                dictionary[word] = []
+            dictionary[word].append(line_num)
+        line_num += 1
+
+    return dictionary
