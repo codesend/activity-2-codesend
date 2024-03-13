40902635e57444a98bd0234977838eef79ca7af7
Daniel Krause <122369776+codesend@users.noreply.github.com>
2024-03-13 02:37:36 -0400

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,11 @@
 def getPrimeNumbers(n):
+    primes = []
+    for num in range(2, n+1):
+        prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                prime = False
+                break
+        if prime:
+            primes.append(num)
+    return primes
